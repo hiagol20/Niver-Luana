ebfedd20fbc8d268a7dac6caa6aeb710d721f39d
Hiago Alves <151949377+hiagol20@users.noreply.github.com>
2024-09-23 11:23:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+// O JavaScript pode ser utilizado para adicionar qualquer comportamento adicional
+// Aqui estamos apenas controlando o momento do surgimento das flores, animado via CSS.
+console.log('Flores estão surgindo, para uma celebração, Feliz aniversário!');
